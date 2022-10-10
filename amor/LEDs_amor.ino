628134b5bcffbca7fdd7d2a35b78f8f1f114b2cb
GypsyLab8 <32037718+gypsylab8@users.noreply.github.com>
2022-10-10 15:35:08 -0300

    Add files via upload

--- amor/LEDs_amor.ino
+++ amor/LEDs_amor.ino
@@ -1,9 +1,11 @@
+int com = A7;
+
 int leds[] = {
-  A0, A1, A2, A3, A4, A5, A6, A7, 3, 5, 6, 9, 10, 11
+  A0, A1, A2, A3, A4, A5, A6, 3, 5, 6, 9, 10, 11
 };
 
 int ledsF[] = {
-  A0, A1, A2, A3, A4, A5, A6, A7
+  A0, A1, A2, A3, A4, A5, A6
 };
 
 int ledsB[] = {
@@ -15,19 +17,20 @@ int ledsL[] = {
 };
 
 int tempLED = 10;
-int tempFase = 3000;
+int tempFase = 1000;
 
 void setup() {
   
   for (int x = 0; x < 11; x++) { 
     pinMode(leds[x], OUTPUT);
   }
-  
+      pinMode(com, INPUT);
+
 }
 
-void loop() {
+void play() {
   
-//P3 ### LED Livros 1 ### 
+//### LED janela ### 
   for(int x = 0; x <3 ; x++){
     for(int y = 0; y < 255; y++) {
        analogWrite(ledsB[0],y);
@@ -43,7 +46,7 @@ void loop() {
        delay(tempLED);
     }
    
-//P3.2 ### LED Livros 2 ### 
+//2 ### LED janela ### 
   for(int x = 0; x <2 ; x++){
     for(int y = 50; y < 255; y++) {
        analogWrite(ledsB[1],y);
@@ -56,79 +59,62 @@ void loop() {
   }
     for(int y = 0; y < 255; y++) {
        analogWrite(ledsB[1],y);
+       analogWrite(ledsB[0],y);
        delay(tempLED);
     }
 
-k  
-//P_1 ### LED ambiente1 ###
+
+//3 ### LED ceu ###
   for(int x = 0; x <2; x++){
     for(int y = 0; y < 255; y++) {
        analogWrite(ledsL[0],y);
        analogWrite(ledsL[1],y/2);
        analogWrite(ledsL[2],y/3);
+       analogWrite(ledsB[2],y);
        delay(tempLED/2);
     }
     for(int y = 255; y > 0; y--) {
        analogWrite(ledsL[0],y);
        analogWrite(ledsL[1],y/2);
        analogWrite(ledsL[2],y/3);
-        delay(tempLED);    
+       analogWrite(ledsB[2],y);
+        delay(tempLED/2);    
     }
   }  
       for(int y = 0; y < 255; y++) {
        analogWrite(ledsL[0],y);
        analogWrite(ledsL[1],y/2);
        analogWrite(ledsL[2],y/3);
-       delay(tempLED);
+       analogWrite(ledsB[2],y);
+       delay(tempLED/2);
       }
       
- delay(tempFase);     
+     
       
-//P_1.2 ### LED ambiente2 ###
-  for(int x = 0; x <2; x++){
+//P4 ### LED ambiente2 ###
+  for(int x = 0; x <3; x++){
     for(int y = 0; y < 31; y++) {
        analogWrite(ledsL[1],y);
+       analogWrite(ledsB[2],y);
        delay(tempLED/2);
     }
     for(int y = 31; y > 0; y--) {
         analogWrite(ledsL[1],y);
-        delay(tempLED);    
+       analogWrite(ledsB[2],y);
+        delay(tempLED/2);    
     }
   }  
       for(int y = 0; y < 255; y++) {
        analogWrite(ledsL[1],y);
+       analogWrite(ledsB[2],y);
+
        delay(tempLED*3);
       }      
-delay(tempFase);   
-
-
- //P_2 ### LED Amebiente3 ###
-    for(int y = 0; y < 50; y++) {
-       analogWrite(ledsL[2],y);
-       delay(tempLED);
-    }
-  for(int x = 0; x <2 ; x++){
-    for(int y = 50; y < 255; y++) {
-       analogWrite(ledsL[2],y);
-       delay(tempLED);
-    }
-    for(int y = 255; y > 50; y--) {
-        analogWrite(ledsL[2],y);
-        delay(tempLED);    
-    }
-  }
-    for(int y = 0; y < 255; y++) {
-       analogWrite(ledsL[2],y);
-       delay(tempLED);
-    }  
-
-
   
 //### Fim ###  
-delay(tempFase*6);  
 for(int y = 255; y > 0; y--) {
     analogWrite(ledsL[0],y);
-    analogWrite(ledsL[1],y/3);
+    analogWrite(ledsL[1],y);
     analogWrite(ledsL[2],y);
     analogWrite(ledsB[0],y);
     analogWrite(ledsB[1],y);
@@ -144,4 +130,11 @@ for(int y = 255; y > 0; y--) {
     
     delay(6000);
 
+}
+
+void loop() {
+  int val = analogRead(com);
+  if (val == HIGH) {
+    play();
+  }
 }
