2f7858f218f458aa710ffefafcb5f8e8e0134eb4
GypsyLab8 <32037718+gypsylab8@users.noreply.github.com>
2022-10-10 15:31:58 -0300

    Add files via upload

--- afeto/LEDs_afeto.ino
+++ afeto/LEDs_afeto.ino
@@ -1,9 +1,18 @@
+#define echoPin A0 // attach pin D2 Arduino to pin Echo of HC-SR04
+#define trigPin A1 //attach pin D3 Arduino to pin Trig of HC-SR04
+// defines variables
+long duration; // variable for the duration of sound wave travel
+int distance; // variable for the distance measurement
+int com = A7;
+int com1 = A6;
+
+
 int leds[] = {
-  A0, A1, A2, A3, A4, A5, A6, A7, 3, 5, 6, 9, 10, 11
+  A0, A1, A2, A3, A4, A5,3, 5, 6, 9, 10, 11
 };
 
 int ledsF[] = {
-  A0, A1, A2, A3, A4, A5, A6, A7
+  A0, A1, A2, A3, A4, A5
 };
 
 int ledsB[] = {
@@ -15,54 +24,155 @@ int ledsL[] = {
 };
 
 int tempLED = 5;
-int tempFase = 100;
+int tempFase = 1000;
 
 void setup() {
   
   for (int x = 0; x < 11; x++) { 
     pinMode(leds[x], OUTPUT);
   }
-  
+   pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
+  pinMode(com, OUTPUT);
+  Serial.begin(9600); // // Serial Communication is starting with 9600 of baudrate speed
+  Serial.println("Ultrasonic Sensor HC-SR04 Test"); // print some text in Serial Monitor
+  Serial.println("with Arduino Nano 2022");   
 }
 
-void loop() {
-//P_1 ### LED Frente ###
-  for(int x = 0; x <2; x++){
-    for(int y = 0; y < 255; y++) {
+void play() {
+//P_1 ### LED Forno ###
+    for(int y = 0; y < 50; y++) {
        analogWrite(ledsL[0],y);
        delay(tempLED*2);
     }
-    for(int y = 255; y > 0; y--) {
+  for(int x = 0; x <6; x++){
+    for(int y = 50; y < 255; y++) {
+       analogWrite(ledsL[0],y);
+       delay(tempLED*2);
+    }
+    for(int y = 255; y > 50; y--) {
         analogWrite(ledsL[0],y);
         delay(tempLED);    
     }
-    for(int y = 0; y < 250; y++) {
-       analogWrite(ledsL[0],y);
-       delay(tempLED*3);
-    }    
   }  
-//### LED Meio Ceu  ###  
-  for(int x = 0; x <3; x++){
-    for(int y = 0; y < 255; y++) {
+      for(int y = 0; y < 150; y++) {
+       analogWrite(ledsL[0],y);
+       delay(tempLED*2);
+      }
+      
+      
+ //P_1.2 ### LED Xicara ###
+    for(int y = 0; y < 50; y++) {
        analogWrite(ledsL[1],y);
        delay(tempLED*2);
     }
-    for(int y = 255; y > 0; y--) {
+  for(int x = 0; x <5 ; x++){
+    for(int y = 50; y < 255; y++) {
+       analogWrite(ledsL[1],y);
+       delay(tempLED);
+    }
+    for(int y = 255; y > 50; y--) {
         analogWrite(ledsL[1],y);
         delay(tempLED);    
     }
-    for(int y = 0; y < 150; y++) {
-       analogWrite(ledsL[1],y);
-       delay(tempLED*3);
-    }    
+  } 
+//### LED Ambiente  ### 
+
+      for(int y = 0; y < 150; y++) {
+       analogWrite(ledsL[1],y); //forno
+       analogWrite(ledsB[0],y);
+       analogWrite(ledsB[1],y/5);
+       analogWrite(ledsB[2],y);       
+       delay(tempLED);
+      }     
+
+    for(int y = 150; y < 255; y++) {
+       analogWrite(ledsL[0],y);
+       delay(tempLED);
+    }
+  for(int x = 0; x <3; x++){
+    for(int y = 150; y < 255; y++) {
+       analogWrite(ledsB[0],y);
+       analogWrite(ledsB[1],y/5);
+       analogWrite(ledsB[2],y);
+       delay(tempLED*2);
+    }
+    for(int y = 255; y > 155; y--) {
+       analogWrite(ledsB[0],y);
+       analogWrite(ledsB[1],y/5);
+       analogWrite(ledsB[2],y);
+        delay(tempLED);    
+    }   
   }    
-analogWrite(ledsL[1],250);
-delay(5000);
+    for(int y = 155; y < 200; y++) {
+       analogWrite(ledsB[0],y);
+       analogWrite(ledsB[1],y/5);
+       analogWrite(ledsB[2],y);
+       delay(tempLED*2);
+    }
+delay(tempFase);
 
-
+//P_1 ### LED Forno ###
+    for(int y = 0; y < 50; y++) {
+       analogWrite(ledsL[0],y);
+       delay(tempLED);
+    }
+  for(int x = 0; x <7; x++){
+    for(int y = 50; y < 255; y++) {
+       analogWrite(ledsL[0],y);
+       delay(tempLED*2);
+    }
+    for(int y = 255; y > 50; y--) {
+       analogWrite(ledsL[0],y);
+       delay(tempLED);
+    }  
+  } 
+  
+//### Fim ###  
+delay(tempFase*3);  
+for(int y = 255; y > 0; y--) {
+    analogWrite(ledsL[0],y);
+    analogWrite(ledsL[1],y);
+    analogWrite(ledsL[2],y);
+    analogWrite(ledsB[0],y);
+    analogWrite(ledsB[1],y/3);
+    analogWrite(ledsB[2],y);        
+    delay(tempLED*2);    
+ }
     analogWrite(ledsL[0],0);
     analogWrite(ledsL[1],0);
     analogWrite(ledsL[2],0);
-    delay(3000);
+    analogWrite(ledsB[0],0);
+    analogWrite(ledsB[1],0);
+    analogWrite(ledsB[2],0);    
+    
+    delay(6000);
 
+}
+
+void loop() {
+ // Clears the trigPin condition
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+  // Displays the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+//Start
+  if(distance <30){
+    Serial.println("play");
+    digitalWrite(com, HIGH);
+    digitalWrite(com1, HIGH);
+    play();
+    
+  }
 }
